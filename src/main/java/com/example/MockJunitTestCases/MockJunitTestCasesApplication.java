5cf3124038f31c14a0e3c3a67134fa5bd8ab7424
KamalakannanKKK <109393676+KamalakannanKKK@users.noreply.github.com>
2024-03-19 17:55:08 +0530

    Add files via upload

---
+++ src/main/java/com/example/MockJunitTestCases/MockJunitTestCasesApplication.java
@@ -0,0 +1,13 @@
+package com.example.MockJunitTestCases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockJunitTestCasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockJunitTestCasesApplication.class, args);
+	}
+
+}
